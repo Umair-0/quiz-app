f55b5ee19b78c1cea4cd70cc1935e7d08a9e0967
Umair Shaikh <117648277+Umair-0@users.noreply.github.com>
2024-01-28 18:02:28 +0530

    Add files via upload

---
+++ Quiz app/index.js
@@ -0,0 +1,126 @@
+const questions = [
+    {
+        questions: "What is the largest ocean in the world?",
+        answers: [
+            { text: "Pacific Ocean", correct: true },
+            { text: "Arctic Ocean", correct: false },
+            { text: "Indian Ocean", correct: false },
+            { text: "Atlantic Ocean", correct: false },
+        ]
+    },
+    {
+        questions: "Which planet is known as the 'Red Planet'?",
+        answers: [
+            { text: "Saturn", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Jupiter", correct: false },
+            { text: "Venus", correct: false },
+        ]
+    },
+    {
+        questions: "What is the currency of Japan?",
+        answers: [
+            { text: "Doller", correct: false },
+            { text: "Rupee", correct: false },
+            { text: "Yen", correct: true },
+            { text: "Euro", correct: false },
+        ]
+    },
+    {
+        questions: "Which is the Smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    },
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.questions;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
+}
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "PLay Again";
+    nextButton.style.display = "block";
+}
+
+
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+
+}
+
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+})
+
+startQuiz();
